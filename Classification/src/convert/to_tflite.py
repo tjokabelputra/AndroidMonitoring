3ff2e8cd1bfebfb7c98bcbb9714594ae117cac35
tjokabelputra <abelputra101@gmail.com>
2026-03-22 11:02:11 +0700

    feat: Logistic Regression Classification

--- Classification/src/convert/to_tflite.py
+++ Classification/src/convert/to_tflite.py
@@ -2,33 +2,54 @@ import os
 import tensorflow as tf
 import Classification.src.common.tools as tools
 
-MODEL = "model.keras"
+ANN_NAME = "ANN.keras"
 ANN_DIR = ""
 ANN_SAVE_DIR = ""
+LR_NAME = "LR.keras"
+LR_DIR = ""
+LR_SAVE_DIR = ""
 
 def add_dir(config):
-    global ANN_DIR, ANN_SAVE_DIR
+    global ANN_DIR, ANN_SAVE_DIR, LR_DIR, LR_SAVE_DIR
 
     ANN_DIR = os.path.expanduser(
-        os.path.join(config["model_dir"], MODEL)
+        os.path.join(config["model_dir"], ANN_NAME)
     )
 
     ANN_SAVE_DIR = os.path.expanduser(
-        os.path.join(config["tflite_dir"], MODEL.replace(".keras", ".tflite"))
+        os.path.join(config["tflite_dir"], ANN_NAME.replace(".keras", ".tflite"))
+    )
+
+    LR_DIR = os.path.expanduser(
+        os.path.join(config["model_dir"], LR_NAME)
+    )
+
+    LR_SAVE_DIR = os.path.expanduser(
+        os.path.join(config["tflite_dir"], LR_NAME.replace(".keras", ".tflite"))
     )
 
     os.makedirs(os.path.dirname(ANN_SAVE_DIR), exist_ok=True)
+    os.makedirs(os.path.dirname(LR_SAVE_DIR), exist_ok=True)
 
 def convert():
-    model = tf.keras.models.load_model(ANN_DIR)
+    model_ann = tf.keras.models.load_model(ANN_DIR)
+    model_lr = tf.keras.models.load_model(LR_DIR)
 
-    converter = tf.lite.TFLiteConverter.from_keras_model(model)
-    tflite_model = converter.convert()
+    converter_ann = tf.lite.TFLiteConverter.from_keras_model(model_ann)
+    converter_lr = tf.lite.TFLiteConverter.from_keras_model(model_lr)
+
+    tflite_model_ann = converter_ann.convert()
+    tflite_model_lr = converter_lr.convert()
 
     with open(ANN_SAVE_DIR, "wb") as f:
-        f.write(tflite_model)
+        f.write(tflite_model_ann)
 
     print("Successfully converted ANN model to TFLite")
+
+    with open(LR_SAVE_DIR, "wb") as f:
+        f.write(tflite_model_lr)
+
+    print("Successfully converted Logistic Regression model to TFLite")
 
 if __name__ == "__main__":
     config = tools.load_config()
--- Classification/src/model/train_lr.py
+++ Classification/src/model/train_lr.py
@@ -1,7 +1,7 @@
 import os
 import Classification.src.common.tools as tools
 import Classification.src.data.dataio as dataio
-from Classification.src.model.ANN import ANN
+from Classification.src.model.LR import LogisticRegression
 
 def train(config):
     train_path = os.path.join(config['save_dir'], 'train.csv')
@@ -10,14 +10,11 @@ def train(config):
     X_train, y_train = dataio.load(train_path)
     X_val, y_val = dataio.load(val_path)
 
-    print("Training ANN")
+    print("Training Logistic Regression")
 
     input_dim = X_train.shape[1]
-    Model = ANN(
+    Model = LogisticRegression(
         input_dim = input_dim,
-        layer=1,
-        neuron=32,
-        dropout_rate=0.0,
         learning_rate=0.001,
     )
 
@@ -28,9 +25,9 @@ def train(config):
         batch_size=32,
     )
 
-    print("Finished Training ANN")
+    print("Finished Training Logistic Regression")
     save_path = os.path.expanduser(
-        os.path.join(config['model_dir'], "ANN.keras")
+        os.path.join(config['model_dir'], "LR.keras")
     )
     os.makedirs(os.path.dirname(save_path), exist_ok=True)
     Model.model.save(save_path)
